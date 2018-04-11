8992b3345db536f331e0ef0bd3c1d765c21775d9
yuanpei666 <13716653073@163.com>
2018-04-11 14:26:49 +0800

    修改taskRunning

--- src/main/java/com/camelot/pmt/task/controller/TaskRunningController.java
+++ src/main/java/com/camelot/pmt/task/controller/TaskRunningController.java
@@ -2,17 +2,16 @@ package com.camelot.pmt.task.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.camelot.pmt.platform.common.ApiResponse;
+import com.camelot.pmt.platform.user.model.UserModel;
 import com.camelot.pmt.platform.utils.DataGrid;
 import com.camelot.pmt.platform.utils.ExecuteResult;
 import com.camelot.pmt.platform.utils.Pager;
 import com.camelot.pmt.task.service.TaskRunningService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -57,5 +56,55 @@ public class TaskRunningController {
         }
     }
 
+
+    /**
+     * <p>
+     * Description:[关闭任务]
+     * </p>
+     *
+     * @param id
+     *          任务id
+     * @return {"status": {"message": "请求处理成功.","code": 200}, "data": {userModel}]
+     */
+    @ApiOperation(value = "实现根据id关闭任务功能", notes = "实现根据id关闭任务功能")
+    @RequestMapping(value = "/updateStatus", method = RequestMethod.POST)
+    public JSONObject updateStatus(
+            @ApiParam(name = "id", value = "任务id", required = true) @RequestParam(required = true) String id) {
+        ExecuteResult<Long> result = new ExecuteResult<Long>();
+        try {
+            result = taskRunningService.updateStatus(id);
+            if (result.isSuccess()) {
+                return ApiResponse.success(result.getResult());
+            }
+            return ApiResponse.error();
+        } catch (Exception e) {
+            return ApiResponse.error();
+        }
+    }
+
+    /**
+     * <p>
+     * Description:[完成任务]
+     * </p>
+     *
+     * @param id
+     *          任务id
+     * @return {"status": {"message": "请求处理成功.","code": 200}, "data": {userModel}]
+     */
+    @ApiOperation(value = "实现任务完成功能", notes = "实现任务完成功能")
+    @RequestMapping(value = "/updateStatusFinish", method = RequestMethod.POST)
+    public JSONObject updateStatusFinish(
+            @ApiParam(name = "id", value = "任务id", required = true) @RequestParam(required = true) String id) {
+        ExecuteResult<Long> result = new ExecuteResult<Long>();
+        try {
+            result = taskRunningService.updateStatusFinish(id);
+            if (result.isSuccess()) {
+                return ApiResponse.success(result.getResult());
+            }
+            return ApiResponse.error();
+        } catch (Exception e) {
+            return ApiResponse.error();
+        }
+    }
 
 }
