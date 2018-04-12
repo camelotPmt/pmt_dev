36e1fb94715517fd5ca69f9800ebf49ad0190e2d
yuanpei666 <13716653073@163.com>
2018-04-12 15:54:50 +0800

    修改taskRunning

--- src/main/java/com/camelot/pmt/task/controller/TaskAlreadyController.java
+++ src/main/java/com/camelot/pmt/task/controller/TaskAlreadyController.java
@@ -6,13 +6,11 @@ import com.camelot.pmt.platform.utils.DataGrid;
 import com.camelot.pmt.platform.utils.ExecuteResult;
 import com.camelot.pmt.platform.utils.Pager;
 import com.camelot.pmt.task.service.TaskAlreadyService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
@@ -38,14 +36,15 @@ public class TaskAlreadyController {
     @RequestMapping(value = "/queryoverdueTaskAlready", method = RequestMethod.GET)
     @ApiImplicitParams({
             @ApiImplicitParam(name = "page", value = "页码", required = true, paramType = "query", dataType = "int"),
-            @ApiImplicitParam(name = "rows", value = "每页数量", required = true, paramType = "query", dataType = "int") })
-    public JSONObject queryoverdueTaskAlready(@ApiIgnore Pager page) {
+            @ApiImplicitParam(name = "rows", value = "每页数量", required = true, paramType = "query", dataType = "int"),
+            @ApiImplicitParam(name = "id", value = "用户id", required = true, paramType = "query", dataType = "Long") })
+    public JSONObject queryoverdueTaskAlready(@ApiIgnore Pager page, @ApiIgnore  Long id) {
         ExecuteResult<DataGrid<Map<String, Object>>> result = new ExecuteResult<DataGrid<Map<String, Object>>>();
         try {
             if (page == null) {
                 return ApiResponse.errorPara();
             }
-            result = taskAlreadyService.queryoverdueTaskAlready(page);
+            result = taskAlreadyService.queryoverdueTaskAlready(page,id);
             if (result.isSuccess()) {
                 return ApiResponse.success(result.getResult());
             }
@@ -55,7 +54,30 @@ public class TaskAlreadyController {
         }
     }
 
-
+    /**
+     * <p>
+     * Description:[重做功能]
+     * </p>
+     *
+     * @param id
+     *          任务id
+     * @return {"status": {"message": "请求处理成功.","code": 200}, "data": {userModel}]
+     */
+    @ApiOperation(value = "实现重做任务功能", notes = "实现重做任务功能")
+    @RequestMapping(value = "/updateRepetitiveOperation;", method = RequestMethod.POST)
+    public JSONObject updateRepetitiveOperation(
+            @ApiParam(name = "id", value = "任务id", required = true) @RequestParam(required = true) Long id) {
+        ExecuteResult<Long> result = new ExecuteResult<Long>();
+        try {
+            result = taskAlreadyService.updateRepetitiveOperation(id);
+            if (result.isSuccess()) {
+                return ApiResponse.success(result.getResult());
+            }
+            return ApiResponse.error();
+        } catch (Exception e) {
+            return ApiResponse.error();
+        }
+    }
 
 
 }
