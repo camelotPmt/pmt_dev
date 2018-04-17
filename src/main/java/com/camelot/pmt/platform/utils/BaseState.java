11f8635243d8c50fafa63fe2774525c7c7fc3c0f
yuanpei666 <13716653073@163.com>
2018-04-17 14:34:35 +0800

    修改taskAlready

---
+++ src/main/java/com/camelot/pmt/platform/utils/BaseState.java
@@ -0,0 +1,7 @@
+package com.camelot.pmt.platform.utils;
+
+public class BaseState {
+	public final static String ZERO = "0"; //激活
+	public final static String ONE = "1";	//禁用
+	public final static String TWO = "2";	//锁定
+}
